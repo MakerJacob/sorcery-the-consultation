e822e2f30400c72188ae90368b026266d00adff3
MakerJacob <106708418+MakerJacob@users.noreply.github.com>
2022-07-13 16:51:44 -0400

    Update pxt.json, main.ts, main.py, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,53 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "card_background":return img`
+eeeeeeeeeeeeeeeeeeeeeeee
+ecccccccccccccccccccccce
+ecccccccccccccccc2222cce
+ecccccccccccccccc2222cce
+ecccccccccccccccccccccce
+eeeeeeeeeeeeeeeeeeeeeeee
+ebbbbbbbbbbbbbbbbbbbbbbe
+ebbbbbbbbbbbbbbbbbbbbbbe
+ebeeeeeeeeeeeeeeeeeeeebe
+ebeeeeeeeeeeeeeeeeeeeebe
+ebeeeeeeeeeeeeeeeeeeeebe
+ebeeeeeeeeeeeeeeeeeeeebe
+ebeeeeeeeeeeeeeeeeeeeebe
+ebeeeeeeeeeeeeeeeeeeeebe
+ebeeeeeeeeeeeeeeeeeeeebe
+ebeeeeeeeeeeeeeeeeeeeebe
+ebeeeeeeeeeeeeeeeeeeeebe
+ebeeeeeeeeeeeeeeeeeeeebe
+ebeeeeeeeeeeeeeeeeeeeebe
+ebeeeeeeeeeeeeeeeeeeeebe
+ebbbbbbbbbbbbbbbbbbbbbbe
+ebbbbbbbbbbbbbbbbbbbbbbe
+eeeeeeeeeeeeeeeeeeeeeeee
+ebbbbbbbbbbbbbbbbbbbbbbe
+ebeeeeeeeeeeeeeeeeeeeebe
+ebeeeeeeeeeeeeeeeeeeeebe
+ebeeeeeeeeeeeeeeeeeeeebe
+ebeeeeeeeeeeeeeeeeeeeebe
+ebeeeeeeeeeeeeeeeeeeeebe
+ebeeeeeeeeeeeeeeeeeeeebe
+ebbbbbbbbbbbbbbbbbbbbbbe
+eeeeeeeeeeeeeeeeeeeeeeee
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,21 +1,4 @@
 //  Now it's python!
 //  To Do:
 //  Create our own card data type
-let card_background = sprites.create(img`
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-`, SpriteKind.Player)
+let card_background = sprites.create(assets.image`card_background`, SpriteKind.Player)
