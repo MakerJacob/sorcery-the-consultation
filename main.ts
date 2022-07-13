753e472a31358d18f5212f1555dcce06cd3ff916
MakerJacob <106708418+MakerJacob@users.noreply.github.com>
2022-07-13 16:54:16 -0400

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
+let my_sprite: Sprite;
 //  Now it's python!
 //  To Do:
 //  Create our own card data type
 let card_background = sprites.create(assets.image`card_background`, SpriteKind.Player)
+for (let i = 0; i < 4; i++) {
+    my_sprite = sprites.create(assets.image`card_background`, SpriteKind.Player)
+    my_sprite.setPosition(50 + i * my_sprite.width, 40)
+}
