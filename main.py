1622e80058f594a1e29e21addd6809e9583f342f
MakerJacob <106708418+MakerJacob@users.noreply.github.com>
2022-07-13 17:38:00 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -13,5 +13,13 @@ for i in range(4):
     my_sprite.set_position(50 + i * my_sprite.width, 40)
 
 
-def createCard(name, health, attack):
-    pass
+
+def createCard(name: string, health: int, attack: int):
+    card = sprites.create(assets.image("""card_background"""), SpriteKind.player)
+    card.set_position(scene.screen_width() / 2, scene.screen_height() / 2)
+    sprites.set_data_string(card, "name", name)
+    sprites.set_data_number(card, "health", health)
+    sprites.set_data_number(card, "attack", attack)
+    
+
+createCard('Potato', 100, 35)
--- main.ts
+++ main.ts
@@ -7,7 +7,12 @@ for (let i = 0; i < 4; i++) {
     my_sprite = sprites.create(assets.image`card_background`, SpriteKind.Player)
     my_sprite.setPosition(50 + i * my_sprite.width, 40)
 }
-function createCard(name: any, health: any, attack: any) {
-    
+function createCard(name: string, health: number, attack: number) {
+    let card = sprites.create(assets.image`card_background`, SpriteKind.Player)
+    card.setPosition(scene.screenWidth() / 2, scene.screenHeight() / 2)
+    sprites.setDataString(card, "name", name)
+    sprites.setDataNumber(card, "health", health)
+    sprites.setDataNumber(card, "attack", attack)
 }
 
+createCard("Potato", 100, 35)
