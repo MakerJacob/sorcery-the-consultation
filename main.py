3b170c20433c070bf919d471236a8878cb24b31c
medeiroman <88601767+medeiroman@users.noreply.github.com>
2022-07-13 17:26:29 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 # Now it's python!
-
-
 # To Do:
 # Create our own card data type
-
-
-card_background = sprites.create(assets.image("""card_background"""), SpriteKind.player)
-
-
+card_background = sprites.create(assets.image("""
+    card_background
+"""), SpriteKind.player)
 for i in range(4):
-    my_sprite = sprites.create(assets.image("""card_background"""), SpriteKind.player)
+    my_sprite = sprites.create(assets.image("""
+        card_background
+    """), SpriteKind.player)
     my_sprite.set_position(50 + i * my_sprite.width, 40)
--- main.ts
+++ main.ts
@@ -2,8 +2,19 @@ let my_sprite: Sprite;
 //  Now it's python!
 //  To Do:
 //  Create our own card data type
-let card_background = sprites.create(assets.image`card_background`, SpriteKind.Player)
+let card_background = sprites.create(assets.image`
+    card_background
+`, SpriteKind.Player)
 for (let i = 0; i < 4; i++) {
-    my_sprite = sprites.create(assets.image`card_background`, SpriteKind.Player)
+    my_sprite = sprites.create(assets.image`
+        card_background
+    `, SpriteKind.Player)
     my_sprite.setPosition(50 + i * my_sprite.width, 40)
+}
+class Card {
+    name: string
+    constructor(name: string): Card {
+        this.name = name
+    }
+    
 }
